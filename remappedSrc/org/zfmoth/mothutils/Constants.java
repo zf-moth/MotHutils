185a72e23911dfc8485514e9b572c18ac67bbc30
MotH <mir0shi@pm.me>
2024-02-13 21:46:24 +0100

    Remove files

--- remappedSrc/org/zfmoth/mothutils/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.zfmoth.mothutils;
-
-import net.minecraft.util.Identifier;
-
-public class Constants {
-    public static final String modid = "mothutils";
-    public static final Identifier GLIDE_SYNC_PACKET_ID = new Identifier(modid, "sync_glide");
-}
